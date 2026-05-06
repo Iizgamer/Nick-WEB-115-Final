d18c7c45292039065b1b63d307843926da72baa7
Nick <nickfoles09@icloud.com>
2026-05-06 14:01:28 -0400

    anhydra snangling pins

--- script.js
+++ script.js
@@ -7,6 +7,14 @@ const craft = document.getElementById('craft');
 const search = document.getElementById('search');
 const inventory = document.getElementById('inventory');
 const inventoryWindow = document.getElementById('inventoryWindow');
+const closeInvBtn = document.getElementById('closeInvBtn');
+const invDisplay = document.getElementById('invDisplay');
+const craftDisplay = document.getElementById('craftDisplay');
+const craftWindow = document.getElementById('craftWindow');
+const closeCraftBtn = document.getElementById('closeCraftBtn');
+const pointer = document.getElementsByClassName('pointer');
+const knife = document.getElementById('knife');
+const campfire = document.getElementById('campfire');
 let searchLvl = 1;
 let craftLvl = 1;
 let btnCount = 0;
@@ -46,6 +54,26 @@ search.addEventListener('click', () => {
 })
 
 inventory.addEventListener('click', ()=>{
-    inventoryWindow.style.display = "block";
-    inventoryWindow.style.backgroundColor = 'black';
+    inventoryWindow.style.display = "grid";
+    invDisplay.textContent = invList;
 })
+
+closeInvBtn.addEventListener('click', ()=>{
+    inventoryWindow.style.display = 'none';
+})
+
+craft.addEventListener('click', ()=>{
+    craftWindow.style.display = 'grid';
+    if (craftLvl == 1) {
+        knife.classList.remove('hideCraft');
+        campfire.classList.remove('hideCraft');
+    }
+})
+
+closeCraftBtn.addEventListener('click', () => {
+    craftWindow.style.display = 'none';
+})
+
+for (i=0; i<pointer.length; i++) {
+    pointer[i].style.cursor = "pointer";
+}
